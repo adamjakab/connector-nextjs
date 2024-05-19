e11cb9c1dbeab3f858dbb894ee2b856c1077f71c
Adam Jakab <adam@jakab.pro>
2024-05-19 15:22:11 +0200

    bundling dist

---
+++ bundler/bundle.ts
@@ -0,0 +1,38 @@
+import * as esbuild from "esbuild";
+import esbuildPluginTsc from "esbuild-plugin-tsc";
+
+// Esbuild configuration
+const getEsbuildOptions = (): esbuild.BuildOptions => {
+  return {
+    entryPoints: ["src/middleware-queueit-connector.ts"],
+    outfile: "dist/index.js",
+    bundle: true,
+    minify: true,
+    platform: "node",
+    target: ["node20"],
+    format: "esm",
+    treeShaking: true,
+    keepNames: true,
+    legalComments: "none",
+    plugins: [
+      esbuildPluginTsc({
+        tsconfigPath: "tsconfig.json",
+      }),
+    ],
+    /** Only the src files will be bundled (not the dependencies)*/
+    external: ["./node_modules/*"],
+    metafile: true,
+    sourcemap: true,
+  };
+};
+
+// Esbuild run
+const runEsbuild = async (): Promise<esbuild.BuildResult> => {
+  const options = getEsbuildOptions();
+  return await esbuild.build(options);
+};
+
+console.log("Bundling...");
+runEsbuild().then((res) => {
+  console.log("Done...");
+});
