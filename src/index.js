f29416c5293788c9b3442c8dea6e17f79587fe7b
Adam Jakab <adam@jakab.pro>
2024-05-19 22:03:19 +0200

    cleanup

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HandleNextjsRequest = void 0;
-var middleware_queueit_connector_1 = require("./connector/middleware-queueit-connector");
-exports.HandleNextjsRequest = middleware_queueit_connector_1.default;
--- src/type/connector-settings.ts
+++ src/type/connector-settings.ts
@@ -1,5 +1,16 @@
+/**
+ * Queue-it connector configuration settings
+ *
+ */
 type ConnectorSettings = {
+  /**
+   * @property customerId - The ID of the account assigned to you in the GO Platform.
+   */
   customerId: string;
+
+  /**
+   * @property secretKey: The secret key from your account to be used for encrypting / decrypting information
+   */
   secretKey: string;
   apiKey: string;
   isEnqueueTokenEnabled: boolean;
